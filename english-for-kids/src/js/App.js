5d93a9a6f5da48b47055ea59527cf7b8ea83d66f
Tamara <yaroslavcevatamara@gmail.com>
2020-12-07 18:09:32 +0300

    feat: add application class

--- english-for-kids/src/js/App.js
+++ english-for-kids/src/js/App.js
@@ -1,4 +1,65 @@
-import '../styles/style.scss';
+/* eslint-disable no-new */
+import cardsData from './cards.json';
+import CardsPage from './CardsPage';
+import MainPage from './MainPage';
+import BurgerMenu from './BurgerMenu';
 
-const div = document.querySelector('.kuku');
-div.innerText = 'lalala';
+export default class App {
+  constructor() {
+    this.mainContainer = document.querySelector('main');
+    this.fullCardsList = cardsData;
+    this.router = this.createRouter();
+    if (window.location.hash === '') {
+      this.showPage('#main');
+    } else {
+      this.showPage(window.location.hash);
+    }
+    window.addEventListener('hashchange', () => {
+      this.showPage(window.location.hash);
+    });
+    this.switchInput = document.querySelector('.switch-input');
+    this.switchInput.addEventListener('change', this.switchMode);
+    new BurgerMenu(this.fullCardsList);
+  }
+
+  createRouter() {
+    const router = {
+      main: new MainPage(this.fullCardsList),
+      // add stats
+    };
+    this.fullCardsList.forEach((elem) => {
+      router[encodeURI(elem.categoryName).toLowerCase()] = new CardsPage(elem);
+    });
+    return router;
+  }
+
+  showPage(hash) {
+    this.mainContainer.innerHTML = '';
+    let name = hash.substring(1);
+    if (!this.router[name]) {
+      name = 'main';
+      window.location.hash = '';
+    }
+    this.mainContainer.append(this.router[name].pageContainer);
+  }
+
+  switchMode = () => {
+    const switchState = this.switchInput.checked;
+    window.dispatchEvent(new CustomEvent('mode-change', {
+      detail: switchState,
+    }));
+    if (switchState) {
+      this.mainContainer.classList.add('play-mode');
+    } else {
+      this.mainContainer.classList.remove('play-mode');
+    }
+  }
+
+  // getData() {
+  //   fetch('./cards.json')
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //     });
+  // }
+}
